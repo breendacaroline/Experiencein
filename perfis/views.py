dd85c33d7fe13854351f33ecc6bbca2f9386b5fe
brenda <brendacaroline011@hotmail.com>
2021-11-08 13:43:46 -0300

    arquivos do terceiro módulo

--- perfis/views.py
+++ perfis/views.py
@@ -1,4 +1,16 @@
 from django.shortcuts import render
+from perfis.models import Perfil
 
 def index(request):
     return render(request, 'index.html')
+
+def exibir(request, perfil_id):
+    perfil = Perfil()
+
+    if perfil_id ==1:
+        perfil = Perfil('Brenda Caroline', 'brendacaroline@gmail.com', '535698', 'IFB')
+
+    if perfil_id == 2:
+        perfil = Perfil('Sabrina', 'sabrinasousa@gmail.com', '587946', 'Amazon')
+
+    return render(request, 'perfil.html', {'perfil' : perfil})
